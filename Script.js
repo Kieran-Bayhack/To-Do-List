9929e97e282c5632d16865c5f2a637ec36bcb29e
Kieran-Bayhack <kieran.bayhack@gmail.com>
2024-05-18 21:46:57 +0200

    added comments

--- Script.js
+++ Script.js
@@ -1,39 +1,39 @@
-const inputBox = document.getElementById("Input-Box");
+const inputBox = document.getElementById("Input-Box"); //calling from the DOM and assigning to variable
 const listContainer = document.getElementById("List-Container");
 
 function addTask(){
     if (inputBox.value === '') {
-        alert("Please enter a task");
+        alert("Please enter a task"); //if nothing is entered prompt error message
     }
     else{
-        let li = document.createElement("li");
+        let li = document.createElement("li"); //populate list
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
+        let span = document.createElement("span"); //adds 'x' for deletion
+        span.innerHTML = "\u00d7"; 
         li.appendChild(span);
     }
-    inputBox.value = "";
-    saveData();
+    inputBox.value = ""; //set text box to empty after adding item
+    saveData(); //call saveData function for local storage
 }
 
-listContainer.addEventListener("click", function (e) {
+listContainer.addEventListener("click", function (e) { //add event listener for clicking on list item
     if (e.target.tagName === "LI") {
-        e.target.classList.toggle("Checked");
+        e.target.classList.toggle("Checked"); //checks list item to show that it is complete
+        saveData(); //calls saveData function to create local storage save of checked list items
+    }
+    else if (e.target.tagName === "SPAN") { //checks if 'X' button has been clicked
+        e.target.parentElement.remove(); //removes list item
         saveData();
     }
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
+}, false); //overides default behaviour
 
-function saveData() {
+function saveData() { //savedata function which creates local storage save
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
 function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data");
+    listContainer.innerHTML = localStorage.getItem("data"); //populates list item with save local storage save data
 }
 
 showTask();
